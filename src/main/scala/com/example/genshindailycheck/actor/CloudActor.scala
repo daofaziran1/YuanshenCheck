836355485a5024fa33cc830654f5e51ef020a0d0
daofaziran1 <liyongzhuang@outlook.com>
2021-10-20 11:41:54 +0800

    去除冗余操作

--- src/main/scala/com/example/genshindailycheck/actor/CloudActor.scala
+++ src/main/scala/com/example/genshindailycheck/actor/CloudActor.scala
@@ -53,63 +53,6 @@ class CloudActor extends Actor {
         .as[Int]
         .getOrElse(0)
       println(s"coin:$coin,free_time:$free_time,total_time:$total_time")
-      val rewardRes = ExecuteGenshinCloudRequest(
-        HttpMethods.GET,
-        Uri(
-          s"${Config.HostUrl}/hk4e_cg_cn/gamer/api/listNotifications?type=NotificationTypePopup&is_sort=true"
-        ),
-        token,
-        device_id,
-        None
-      )
-      val rewards = parse(rewardRes)
-        .getOrElse(Json.Null)
-        .hcursor
-        .downField("data")
-        .downField("list")
-        .as[List[Json]]
-        .getOrElse(List())
-      rewards
-        .foreach(json => {
-          val reward_id = json.hcursor.downField("id").as[String].getOrElse("")
-          val status = json.hcursor.downField("status").as[String].getOrElse("")
-          val rewardRes = ExecuteGenshinCloudRequest(
-            HttpMethods.POST,
-            Uri(s"${Config.HostUrl}/hk4e_cg_cn/gamer/api/ackNotification"),
-            token,
-            device_id,
-            Some(HttpEntity(Map("id" -> reward_id).asJson.noSpaces))
-          )
-          println(rewardRes)
-        })
-      walletRes = parse(
-        ExecuteGenshinCloudRequest(
-          HttpMethods.GET,
-          Uri(s"${Config.HostUrl}/hk4e_cg_cn/wallet/wallet/get"),
-          token,
-          device_id,
-          None
-        )
-      ).getOrElse(Json.Null)
-      coin = walletRes.hcursor
-        .downField("data")
-        .downField("coin")
-        .downField("coin_num")
-        .as[Int]
-        .getOrElse(0)
-      free_time = walletRes.hcursor
-        .downField("data")
-        .downField("free_time")
-        .downField("free_time")
-        .as[Int]
-        .getOrElse(0)
-      total_time = walletRes.hcursor
-        .downField("data")
-        .downField("total_time")
-        .downField("total_time")
-        .as[Int]
-        .getOrElse(0)
-      println(s"coin:$coin,free_time:$free_time,total_time:$total_time")
     }
     case _ => println("cloud")
   }
